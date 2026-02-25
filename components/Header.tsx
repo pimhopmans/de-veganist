d696718ae07b3cc96563ef23ea305c0abb85c786
Pim Hopmans <pim.hopmans@live.nl>
2026-02-25 21:12:52 +0100

    add logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,24 @@
 "use client";
 import Link from "next/link";
 import { scrollTo } from "@/lib/window/scroll";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
 
 export default function Header() {
   return (
     <header className="border-b border-b-stone-200">
       <div className="mx-auto max-w-4xl flex items-center justify-between p-4">
-        <Link href="/" className="text-2xl font-bold text-green-600">
-          De Veganist
+        <Link href="/" className="w-48">
+          <Image src={logo} alt="De Veganist logo" />
         </Link>
 
         <nav className="space-x-4">
-          <button onClick={() => scrollTo("recepten")}>Recepten</button>
+          <button
+            className="hover:underline"
+            onClick={() => scrollTo("recepten")}
+          >
+            Recepten
+          </button>
         </nav>
       </div>
     </header>
